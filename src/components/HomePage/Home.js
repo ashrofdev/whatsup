0a6a8c8d1958365e959e491afc86a99cbd4c3968
First Last <salmanashraf853@gmail.com>
2019-12-06 23:50:19 +0100

    starting the project

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -9,6 +9,14 @@ const Home = () => {
                     <img />
                 </figure>
                 <h2>User Name</h2>
+                <nav>
+                    <ul>
+                        <li>My Profile</li>
+                        <li>Followers {20}</li>
+                        <li>following {15}</li>
+                        <li>Notification</li>
+                    </ul>
+                </nav>
             </header>
         </div>
     );
