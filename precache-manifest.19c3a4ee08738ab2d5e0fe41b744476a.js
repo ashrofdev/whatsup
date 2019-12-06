ee296dd5f5926c745b4c5ffcf471dc4260119161
First Last <salmanashraf853@gmail.com>
2019-12-06 12:35:43 +0100

    Updates

--- precache-manifest.19c3a4ee08738ab2d5e0fe41b744476a.js
+++ precache-manifest.19c3a4ee08738ab2d5e0fe41b744476a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b9ee277d9dd72aaf500649d485ee819e",
+    "revision": "0b256e077c4c3d6f085439fb96bea5ee",
     "url": "/whatsup/index.html"
   },
   {
-    "revision": "f85e12c5f010d5a64159",
-    "url": "/whatsup/static/css/main.7e683086.chunk.css"
+    "revision": "ce4baf5f60ce49a0605f",
+    "url": "/whatsup/static/css/main.fe42e350.chunk.css"
   },
   {
-    "revision": "0376184c84f0a4030078",
-    "url": "/whatsup/static/js/2.9ff93301.chunk.js"
+    "revision": "4c3917208f6bd75e28fe",
+    "url": "/whatsup/static/js/2.132359c9.chunk.js"
   },
   {
-    "revision": "f85e12c5f010d5a64159",
-    "url": "/whatsup/static/js/main.85f39f07.chunk.js"
+    "revision": "ce4baf5f60ce49a0605f",
+    "url": "/whatsup/static/js/main.8db3df7e.chunk.js"
   },
   {
     "revision": "c5aafbd13e26ca082a02",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/whatsup/precache-manifest.fe5f7e35887309a8238a18cd3cd80fee.js"
+  "/whatsup/precache-manifest.19c3a4ee08738ab2d5e0fe41b744476a.js"
 );
 
 self.addEventListener('message', (event) => {
